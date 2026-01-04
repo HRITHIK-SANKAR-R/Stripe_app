63373c9361afd3bcf74f8881a6991822708085bd
HRITHIK-SANKAR-R <hrithiksrr@gmail.com>
2026-01-04 13:45:52 +0530

    Added signup and login html for accounts and redirection form signin and logout

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.views import LogoutView
+from django.urls import path
+
+from .views import SignUpView
+urlpatterns = [
+    path('',include('django.contrib.auth.urls')),
+    path('signup/',SignUpView.as_view(),name='signup'),
+    path('logout/',LogoutView.as_view(),name='logout'),
+]
